ee652b4f2a9abb780a7ce4977b01a879ab21d898
Tian Jiang <riersy2324@gmail.com>
2021-08-09 18:22:42 +0800

    change kmeans algo

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-var colorArr = [];
-
+var centroids = []
+    // var data = [];
 $("#file-input").change(function(e) {
     var file = e.target.files[0]
     var reader = new FileReader();
     reader.onload = fileOnload;
     reader.readAsDataURL(file);
+    // console.log('d1: ' + fileOnload())
+    // kMeans(data)
 });
+
 var canvas = $("#canvas")[0];
 var context = canvas.getContext("2d");
 
@@ -13,30 +16,28 @@ function fileOnload(e) {
 
     var $img = $("<img>", { src: e.target.result });
     $img.on("load", function() {
-        var w, h;
+        var w, h, csize = 500;
 
-        if (this.naturalWidth <= 300 && this.naturalHeight <= 300) {
-            w = this.naturalWidth;
-            h = this.naturalHeight;
-        } else if (this.naturalWidth / this.naturalHeight >= 1) {
-            w = 300;
-            h = (this.naturalHeight / this.naturalWidth) * 300;
+        if (this.naturalWidth / this.naturalHeight >= 1) {
+            w = csize;
+            h = (this.naturalHeight / this.naturalWidth) * csize;
         } else {
-            h = 300;
-            w = (this.naturalWidth / this.naturalHeight) * 300;
+            h = csize;
+            w = (this.naturalWidth / this.naturalHeight) * csize;
         }
         canvas.width = w;
         canvas.height = h;
         context.drawImage(this, 0, 0, w, h);
         var imgData = context.getImageData(0, 0, canvas.width, canvas.height);
         var oData = imgData.data;
-        console.log(oData)
-
+        console.log(oData.slice(0, 3))
+        console.log(typeof oData.slice(0, 3))
         var data = [];
         for (var i = 0; i < oData.length; i = i + 4) {
             data.push(oData.slice(i, i + 3))
         }
-        console.log('data0' + data[0])
+        console.log('d0: ' + data[0])
+        console.log(typeof data[0])
         $("#canvas").on("click", function(e) {
             var pos = findPos(this);
             var x = e.pageX - pos.x;
@@ -48,17 +49,9 @@ function fileOnload(e) {
             $('#output').html("<p>HEX: #" + hex + "</br>" + "RGB: " + oData[i] + "," + oData[i + 1] + "," + oData[i + 2] + "</p>").css("background-color", "#" + hex)
         })
 
-        var index = 5;
         $("#palette-container").empty()
-        console.log("clear")
-        kMeans(index, data)
-
-        for (var i = 0; i < index; i++) {
-            var hex = rgbToHex(colorArr[i][0], colorArr[i][1], colorArr[i][2])
-                // $("#palette-container").append('<div class="palette-box" style="background-color:' + '#' + hex + '"/>' + 'HEX: #' + hex + '</br>' + 'RGB: ' + colorArr[i].toString(", ") + '</div>')
-            $("#palette-container").append('<div class="palette-box" style="background-color:' + '#' + hex + '"/></div>')
-        }
-
+        console.log(data)
+        kMeans(data)
     });
 }
 
@@ -80,176 +73,222 @@ function rgbToHex(r, g, b) {
     return ((r << 16) | (g << 8) | b).toString(16);
 }
 
-function kMeans(k, data) {
-    var means = [];
-    var assignments = [];
-    var dataExtremes;
-    var dataRange;
+// function kMeans(data, k = 5) {
+//     var means = [];
+//     var assignments = [];
+//     var dataExtremes;
+//     var dataRange;
 
-    function kmeans() {
+//     function kmeans() {
 
-        dataExtremes = getDataExtremes(data);
-        dataRange = getDataRanges(dataExtremes);
-        means = initMeans(k);
+//         dataExtremes = getDataExtremes(data);
+//         dataRange = getDataRanges(dataExtremes);
+//         means = initMeans(k);
 
-        makeAssignments();
-        run()
-    }
+//         makeAssignments();
+//         run()
+//         console.log(colorArr.toString())
+//         for (var i = 0; i < 5; i++) {
+//             var hex = ("000000" + rgbToHex(colorArr[i][0], colorArr[i][1], colorArr[i][2])).slice(-6)
+//             $("#palette-container").append('<div class="palette-box" style="background-color:' + '#' + hex + '"/></div>')
+//         }
+//     }
 
-    function getDataRanges(extremes) {
-        var ranges = [];
+//     function getDataRanges(extremes) {
+//         var ranges = [];
 
-        for (var dimension in extremes) {
-            ranges[dimension] = extremes[dimension].max - extremes[dimension].min;
+//         for (var dimension in extremes) {
+//             ranges[dimension] = extremes[dimension].max - extremes[dimension].min;
+//         }
+//         console.log("ranges: " + ranges)
+//         return ranges;
+//     }
+
+//     function getDataExtremes(data) {
+
+//         var extremes = [];
+
+//         for (var i in data) {
+//             var point = data[i];
+//             for (var dimension in point) {
+//                 if (!extremes[dimension]) {
+//                     extremes[dimension] = { min: 1000, max: 0 };
+//                 }
+
+//                 if (point[dimension] < extremes[dimension].min) {
+//                     extremes[dimension].min = point[dimension];
+//                 }
+
+//                 if (point[dimension] > extremes[dimension].max) {
+//                     extremes[dimension].max = point[dimension];
+//                 }
+//             }
+//         }
+//         console.log("extremes: " + extremes)
+//         return extremes;
+
+//     }
+
+//     function initMeans(k) {
+
+//         while (k--) {
+//             var mean = [];
+
+//             for (var dimension in dataExtremes) {
+//                 mean[dimension] = dataExtremes[dimension].min + (Math.random() * dataRange[dimension]);
+//             }
+
+//             means.push(mean);
+//         }
+//         console.log("means: " + means)
+//         return means;
+
+//     };
+
+//     function makeAssignments() {
+
+//         for (var i in data) {
+//             var point = data[i];
+//             var distances = [];
+
+//             for (var j in means) {
+//                 var mean = means[j];
+//                 var sum = 0;
+
+//                 for (var dimension in point) {
+//                     var difference = point[dimension] - mean[dimension];
+//                     difference *= difference;
+//                     sum += difference;
+//                 }
+
+//                 distances[j] = Math.sqrt(sum);
+//             }
+
+//             assignments[i] = distances.indexOf(Math.min.apply(null, distances));
+//         }
+
+//     }
+
+//     function moveMeans() {
+
+//         makeAssignments();
+
+//         var sums = Array(means.length);
+//         var counts = Array(means.length);
+//         var moved = false;
+
+//         for (var j in means) {
+//             counts[j] = 0;
+//             sums[j] = Array(means[j].length);
+//             for (var dimension in means[j]) {
+//                 sums[j][dimension] = 0;
+//             }
+//         }
+
+//         for (var point_index in assignments) {
+//             var mean_index = assignments[point_index];
+//             var point = data[point_index];
+//             var mean = means[mean_index];
+
+//             counts[mean_index]++;
+
+//             for (var dimension in mean) {
+//                 sums[mean_index][dimension] += point[dimension];
+//             }
+//         }
+
+//         for (var mean_index in sums) {
+//             // console.log(counts[mean_index]);
+//             if (0 === counts[mean_index]) {
+//                 sums[mean_index] = means[mean_index];
+//                 console.log("Mean with no points");
+//                 console.log(sums[mean_index]);
+
+//                 for (var dimension in dataExtremes) {
+//                     sums[mean_index][dimension] = dataExtremes[dimension].min + (Math.random() * dataRange[dimension]);
+//                 }
+//                 continue;
+//             }
+
+//             for (var dimension in sums[mean_index]) {
+//                 sums[mean_index][dimension] /= counts[mean_index];
+//             }
+//         }
+
+//         if (means.toString() !== sums.toString()) {
+//             moved = true;
+//         }
+
+//         means = sums;
+//         return moved;
+
+//     }
+
+//     function run() {
+//         var x, y, z;
+//         var moved = moveMeans();
+
+//         if (moved) {
+//             run()
+//         } else {
+//             colorArr = []
+//             for (var i in means) {
+//                 var point = means[i];
+//                 colorArr.push(point.map(x => Math.round(x) > 255 ? 255 : Math.round(x)))
+//                 x = point[0];
+//                 y = point[1];
+//                 z = point[2];
+//                 console.log(`!!!: ${Math.round(x)}, ${Math.round(y)}, ${Math.round(z)}`)
+//             }
+//         }
+
+//     }
+
+//     kmeans();
+// }
+
+const kMeans = (data, k = 5) => {
+
+    const cent = data.slice(0, k);
+    console.log(cent)
+    const distances = Array.from({ length: data.length }, () =>
+        Array.from({ length: k }, () => 0)
+    );
+    const classes = Array.from({ length: data.length }, () => -1);
+    let itr = true;
+
+    while (itr) {
+        itr = false;
+
+        for (let d in data) {
+            for (let c = 0; c < k; c++) {
+                distances[d][c] = Math.hypot(
+                    ...Object.keys(data[0]).map(key => data[d][key] - cent[c][key])
+                );
+            }
+            const m = distances[d].indexOf(Math.min(...distances[d]));
+            if (classes[d] !== m) itr = true;
+            classes[d] = m;
         }
-        console.log("ranges: " + ranges)
-        return ranges;
-    }
 
-    function getDataExtremes(points) {
-
-        var extremes = [];
-
-        for (var i in data) {
-            var point = data[i];
-            for (var dimension in point) {
-                if (!extremes[dimension]) {
-                    extremes[dimension] = { min: 1000, max: 0 };
+        for (let c = 0; c < k; c++) {
+            centroids[c] = Array.from({ length: data[0].length }, () => 0);
+            const size = data.reduce((acc, _, d) => {
+                if (classes[d] === c) {
+                    acc++;
+                    for (let i in data[0]) centroids[c][i] += data[d][i];
                 }
-
-                if (point[dimension] < extremes[dimension].min) {
-                    extremes[dimension].min = point[dimension];
-                }
-
-                if (point[dimension] > extremes[dimension].max) {
-                    extremes[dimension].max = point[dimension];
-                }
+                return acc;
+            }, 0);
+            for (let i in data[0]) {
+                centroids[c][i] = Math.round(Number(centroids[c][i] / size).toFixed(2));
             }
-        }
-        console.log("extremes: " + extremes)
-        return extremes;
-
-    }
-
-    function initMeans(k) {
-
-        if (!k) {
-            k = 3;
-        }
-
-        while (k--) {
-            var mean = [];
-
-            for (var dimension in dataExtremes) {
-                mean[dimension] = dataExtremes[dimension].min + (Math.random() * dataRange[dimension]);
-            }
-
-            means.push(mean);
-        }
-        console.log("means: " + means)
-        return means;
-
-    };
-
-    function makeAssignments() {
-
-        for (var i in data) {
-            var point = data[i];
-            var distances = [];
-
-            for (var j in means) {
-                var mean = means[j];
-                var sum = 0;
-
-                for (var dimension in point) {
-                    var difference = point[dimension] - mean[dimension];
-                    difference *= difference;
-                    sum += difference;
-                }
-
-                distances[j] = Math.sqrt(sum);
-            }
-
-            assignments[i] = distances.indexOf(Math.min.apply(null, distances));
         }
 
     }
+    centroids.forEach(x => x > 255 ? 255 : x)
+        // document.write(centroids[0] + '</br>' + centroids)
+    console.log(centroids)
+    console.log(classes)
 
-    function moveMeans() {
-
-        makeAssignments();
-
-        var sums = Array(means.length);
-        var counts = Array(means.length);
-        var moved = false;
-
-        for (var j in means) {
-            counts[j] = 0;
-            sums[j] = Array(means[j].length);
-            for (var dimension in means[j]) {
-                sums[j][dimension] = 0;
-            }
-        }
-
-        for (var point_index in assignments) {
-            var mean_index = assignments[point_index];
-            var point = data[point_index];
-            var mean = means[mean_index];
-
-            counts[mean_index]++;
-
-            for (var dimension in mean) {
-                sums[mean_index][dimension] += point[dimension];
-            }
-        }
-
-        for (var mean_index in sums) {
-            // console.log(counts[mean_index]);
-            if (0 === counts[mean_index]) {
-                sums[mean_index] = means[mean_index];
-                console.log("Mean with no points");
-                console.log(sums[mean_index]);
-
-                for (var dimension in dataExtremes) {
-                    sums[mean_index][dimension] = dataExtremes[dimension].min + (Math.random() * dataRange[dimension]);
-                }
-                continue;
-            }
-
-            for (var dimension in sums[mean_index]) {
-                sums[mean_index][dimension] /= counts[mean_index];
-            }
-        }
-
-        if (means.toString() !== sums.toString()) {
-            moved = true;
-        }
-
-        means = sums;
-        return moved;
-
-    }
-
-    function run() {
-        var x, y, z;
-        var moved = moveMeans();
-
-        if (moved) {
-            run()
-        } else {
-            colorArr = []
-            for (var i in means) {
-                var point = means[i];
-                colorArr.push(point.map(x => Math.round(x) > 255 ? 255 : Math.round(x)))
-                x = point[0];
-                y = point[1];
-                z = point[2];
-                console.log(`!!!: ${Math.round(x)}, ${Math.round(y)}, ${Math.round(z)}`)
-            }
-        }
-        console.log(colorArr.toString())
-        return colorArr;
-    }
-
-    kmeans();
-}
+};
